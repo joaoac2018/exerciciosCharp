803b05ec83cf42f5b701b9c18f4ded0259420f37
joaoac2018 <jpaulocruz285@gmail.com>
2021-02-18 22:04:59 -0300

    Commit inicial

---
+++ Calculadora/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1, numero2;
+            char operacao;
+
+            Console.WriteLine("Digite o primeiro numero");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero");
+            numero2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Escolha a operação (+ - * / ): ");
+            operacao = char.Parse(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (operacao)
+            {
+                default:
+                    Console.WriteLine("Erro, opção inválida");
+                break;
+
+                case '+':
+                     resultado = numero1 + numero2;
+                     Console.WriteLine("O resultado da soma é: " + resultado);
+                     break;
+                case '-':
+                     resultado = numero1 - numero2;
+                     Console.WriteLine("O resultado da subtração: " + resultado);
+                     break;
+                case 'x':
+                     resultado = numero1 * numero2;
+                     Console.WriteLine("O resultado da multiplicação é: ");
+                     break;
+                case '/':
+                     resultado = numero1 * numero2;
+                     Console.WriteLine("O resultado da divisão é: ");
+                     break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
